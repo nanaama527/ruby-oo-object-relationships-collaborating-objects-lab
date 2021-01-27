2482ae2e02d3c3176ce80ccafd1ee46d6851b221
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-27 18:51:57 -0500

    hi

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def self.find_or_create_by_name(name)
+        @@all.each do |artist|
+            if artist.name = name
+                return artist
+            end
+            artist = self.new(name)
+            artist
+        end
+    end
+
+    def print_songs
+        self.songs.each do |song|
+            puts "#{song.name}"
+        end
+    end
+end
